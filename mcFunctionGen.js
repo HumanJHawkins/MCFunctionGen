50b8f34568b08d345861434cc68339d31493094b
HumanJHawkins <jhawkins128@gmail.com>
2019-11-02 17:10:43 -0700

    Usable pre-alpha. Added torch plain function.

--- mcFunctionGen.js
+++ mcFunctionGen.js
@@ -1,93 +1,244 @@
 let wallType, wallMaterial;
 let preserveMinerals;
 
+let bottomLeftX, bottomLeftY, bottomLeftZ;
+let topRightX, topRightY, topRightZ;
+let spacing;
+
 let centerX, floorY, centerZ;
 let lengthX, heightY, widthZ;
+
+// Directions
+let directions = ["north", "east", "south", "west"];
 
 let minX, maxX;
 let minY, maxY;
 let minZ, maxZ;
 
-// Directions to match minecraft
-let south = 0;
-let west = 1;
-let north = 2;
-let east = 3;
-
-let codeOutput = "";
+let codeOutput;
 
 function initValues() {
     wallType = document.getElementById("wallType").value;
     wallMaterial = document.getElementById("wallMaterial").value;
 
     preserveMinerals = document.getElementById("preserveMinerals").checked;
+
+    bottomLeftX = parseInt(document.getElementById("bottomLeftX").value);
+    bottomLeftY = parseInt(document.getElementById("bottomLeftY").value);
+    bottomLeftZ = parseInt(document.getElementById("bottomLeftZ").value);
+
+    topRightX = parseInt(document.getElementById("topRightX").value);
+    topRightY = parseInt(document.getElementById("topRightY").value);
+    topRightZ = parseInt(document.getElementById("topRightZ").value);
+
+    spacing = parseInt(document.getElementById("spacing").value);
+
     centerX = parseInt(document.getElementById("centerX").value);
     floorY = parseInt(document.getElementById("floorY").value);
     centerZ = parseInt(document.getElementById("centerZ").value);
+
     lengthX = parseInt(document.getElementById("lengthX").value);
     heightY = parseInt(document.getElementById("heightY").value);
     widthZ = parseInt(document.getElementById("widthZ").value);
 
     codeOutput = "";
 }
+
 // fill -48 9 -48 48 9 48 minecraft:stone
+
+function torchPlain() {
+    initValues();
+    // alert('In torchPLain)\nNums: ' +
+    //     bottomLeftX + '\n' +
+    //     bottomLeftY + '\n' +
+    //     bottomLeftZ + '\n' +
+    //     topRightX + '\n' +
+    //     topRightY + '\n' +
+    //     topRightZ + '\n' +
+    //     spacing + '\n');
+    let code = '';
+
+    for(let i = bottomLeftX; i<= topRightX; i+= spacing) {
+        for(let j = bottomLeftY; j <= topRightY; j += spacing) {
+            for(let k = bottomLeftZ; k <= topRightZ; k += spacing) {
+                code += 'setblock ' + i + ' ' + j + ' ' + k + ' minecraft:torch keep\n';
+            }
+        }
+    }
+
+     document.getElementById("codeOutput").value = code;
+}
+
+
 
 function dwarvenStairs() {
     initValues();
 
-    for(let i = centerX - 4; i<=41; i+=8) {
-        codeOutput += singleDwarvenStair(i, floorY + 4, centerZ - 4, north);
-    }
-
-    for(let i = centerX; i<=37; i+=8) {
-        codeOutput += singleDwarvenStair(i, floorY, centerZ, north);
-    }
-
-    for(let i = centerX + 4; i<=33; i+=8) {
-        codeOutput += singleDwarvenStair(i, floorY -4, centerZ + 4, north);
-    }
-
+    codeOutput = dwarvenFourSquare(centerX, floorY, centerZ);
     document.getElementById("codeOutput").value = codeOutput;
 }
 
+function dwarvenFourSquare() {
+    let code = "";
+
+    // North
+    code += '##############################################################################################\n';
+    code += '# North at ' + centerX + ' by ' + centerZ + '\n';
+    for (let i = centerX; i <= 37; i += 8) {
+        code += singleDwarvenStair(i, floorY, centerZ, directions[0]);
+    }
+    for (let i = centerX + 4; i <= 33; i += 8) {
+        code += singleDwarvenStair(i, floorY - 4, centerZ + 4, directions[0]);
+    }
+
+    code += '##############################################################################################\n';
+    code += '# East at ' + -centerX + ' by ' + centerZ + '\n';
+    // East
+    for (let i = -centerX; i >= -37; i -= 8) {
+        code += singleDwarvenStair(i, floorY, centerZ, directions[1]);
+    }
+    for (let i = -centerX - 4; i >= -33; i -= 8) {
+        code += singleDwarvenStair(i, floorY - 4, centerZ + 4, directions[1]);
+    }
+
+    code += '##############################################################################################\n';
+    code += '# South at ' + -centerX + ' by ' + -centerZ + '\n';
+    // South
+    for (let i = -centerX; i >= -37; i -= 8) {
+        code += singleDwarvenStair(i, floorY, -centerZ, directions[2]);
+    }
+    for (let i = -centerX - 4; i >= -33; i -= 8) {
+        code += singleDwarvenStair(i, floorY - 4, -centerZ - 4, directions[2]);
+    }
+
+    code += '##############################################################################################\n';
+    code += '# West at ' + centerX + ' by ' + -centerZ + '\n';
+    // West
+    for (let i = centerX; i <= 37; i += 8) {
+        code += singleDwarvenStair(i, floorY, -centerZ, directions[3]);
+    }
+    for (let i = centerX + 4; i <= 33; i += 8) {
+        code += singleDwarvenStair(i, floorY - 4, -centerZ - 4, directions[3]);
+    }
+
+    return code;
+}
+
+function arrayRotate(array, count, modifyOriginal = false) {
+    if (!modifyOriginal) array = array.concat();
+
+    count -= array.length * Math.floor(count / array.length);
+    array.push.apply(array, array.splice(0, count));
+    return array;
+}
+
 function singleDwarvenStair(x, y, z, direction) {
+    // Function originally written for stairs rising toward north. So morphing coords for other directions.
 
-    code  = 'fill ' + x + ' ' + y + ' ' + z + ' ' + (x+6) + ' ' + y + ' ' + z + ' minecraft:air replace minecraft:stone_bricks\n';
-    code += 'fill ' + (x+1) + ' ' + (y-1) + ' ' + z + ' ' + (x+5) + ' ' + (y-1) + ' ' + z + ' minecraft:air replace minecraft:stone_bricks\n';
-    code += 'fill ' + (x+2) + ' ' + (y-2) + ' ' + z + ' ' + (x+4) + ' ' + (y-2) + ' ' + z + ' minecraft:air replace minecraft:stone_bricks\n';
-    code += 'fill ' + (x+1) + ' ' + y + ' ' + (z-1) + ' ' + (x+5) + ' ' + y + ' ' + (z-1) + ' minecraft:air replace minecraft:stone_bricks\n';
-    code += 'fill ' + (x+2) + ' ' + y + ' ' + (z-2) + ' ' + (x+4) + ' ' + y + ' ' + (z-2) + ' minecraft:air replace minecraft:stone_bricks\n';
-    code += 'fill ' + (x+2) + ' ' + (y-1) + ' ' + (z-1) + ' ' + (x+4) + ' ' + (y-1) + ' ' + (z-1) + ' minecraft:air replace minecraft:stone_bricks\n';
+    // Handle block rotation based on input direction... Here, just defining north as south if rotated for example.
+    let rotDir = arrayRotate(directions, -directions.indexOf(direction));
 
-    code += 'setblock ' + (x) + ' ' + (y+1) + ' ' + (z) + ' minecraft:torch keep\n';
-    code += 'setblock ' + (x-4) + ' ' + (y-1) + ' ' + (z+1) + ' minecraft:torch[facing=south] keep\n';
+    //  Handle north/south or east/west reversal of relative positions
+    let xMod = 1;
+    let zMod = 1;
+    let temp = 0;
 
-    code += 'setblock ' + (x) + ' ' + (y) + ' ' + (z) + ' minecraft:stone_brick_stairs[facing=west] keep\n';
-    code += 'setblock ' + (x+1) + ' ' + (y-1) + ' ' + (z) + ' minecraft:stone_brick_stairs[facing=west] keep\n';
-    code += 'setblock ' + (x+1) + ' ' + (y) + ' ' + (z-1) + ' minecraft:stone_brick_stairs[facing=west] keep\n';
-    code += 'setblock ' + (x+1) + ' ' + (y-3) + ' ' + (z+1) + ' minecraft:stone_brick_stairs[facing=east] keep\n';
+    if (direction === "south" || direction === "east") {
+        xMod = -xMod;
+        zMod = -zMod;
+    }
 
-    code += 'setblock ' + (x+6) + ' ' + (y) + ' ' + (z) + ' minecraft:stone_brick_stairs[facing=east] keep\n';
-    code += 'setblock ' + (x+5) + ' ' + (y-1) + ' ' + (z) + ' minecraft:stone_brick_stairs[facing=east] keep\n';
-    code += 'setblock ' + (x+5) + ' ' + (y) + ' ' + (z-1) + ' minecraft:stone_brick_stairs[facing=east] keep\n';
-    code += 'setblock ' + (x+5) + ' ' + (y-3) + ' ' + (z+1) + ' minecraft:stone_brick_stairs[facing=west] keep\n';
+    if (direction === "west" || direction === "east") {
+        temp = x;
+        x = z;
+        z = temp;
+    }
 
-    code += 'setblock ' + (x+2) + ' ' + (y-3) + ' ' + (z+1) + ' minecraft:stone_brick_stairs[facing=north] keep\n';
-    code += 'setblock ' + (x+3) + ' ' + (y-3) + ' ' + (z+1) + ' minecraft:stone_brick_stairs[facing=north] keep\n';
-    code += 'setblock ' + (x+4) + ' ' + (y-3) + ' ' + (z+1) + ' minecraft:stone_brick_stairs[facing=north] keep\n';
+    let code = "\n\n";
 
-    code += 'setblock ' + (x+2) + ' ' + (y-2) + ' ' + (z) + ' minecraft:stone_brick_stairs[facing=north] keep\n';
-    code += 'setblock ' + (x+3) + ' ' + (y-2) + ' ' + (z) + ' minecraft:stone_brick_stairs[facing=north] keep\n';
-    code += 'setblock ' + (x+4) + ' ' + (y-2) + ' ' + (z) + ' minecraft:stone_brick_stairs[facing=north] keep\n';
+    if (direction === "north" || direction === "south")
+    {
 
-    code += 'setblock ' + (x+2) + ' ' + (y-1) + ' ' + (z-1) + ' minecraft:stone_brick_stairs[facing=north] keep\n';
-    code += 'setblock ' + (x+3) + ' ' + (y-1) + ' ' + (z-1) + ' minecraft:stone_brick_stairs[facing=north] keep\n';
-    code += 'setblock ' + (x+4) + ' ' + (y-1) + ' ' + (z-1) + ' minecraft:stone_brick_stairs[facing=north] keep\n';
+        code += 'fill ' + (x + 0 * xMod) + ' ' + (y - 0) + ' ' + (z + 0 * zMod) + ' ' + (x + 6 * xMod) + ' ' + (y - 0) + ' ' + (z + 0 * zMod) + ' minecraft:air replace minecraft:stone_bricks\n';
+        code += 'fill ' + (x + 1 * xMod) + ' ' + (y - 1) + ' ' + (z + 0 * zMod) + ' ' + (x + 5 * xMod) + ' ' + (y - 1) + ' ' + (z + 0 * zMod) + ' minecraft:air replace minecraft:stone_bricks\n';
+        code += 'fill ' + (x + 2 * xMod) + ' ' + (y - 2) + ' ' + (z + 0 * zMod) + ' ' + (x + 4 * xMod) + ' ' + (y - 2) + ' ' + (z + 0 * zMod) + ' minecraft:air replace minecraft:stone_bricks\n';
+        code += 'fill ' + (x + 1 * xMod) + ' ' + (y - 0) + ' ' + (z - 1 * zMod) + ' ' + (x + 5 * xMod) + ' ' + (y - 0) + ' ' + (z - 1 * zMod) + ' minecraft:air replace minecraft:stone_bricks\n';
+        code += 'fill ' + (x + 2 * xMod) + ' ' + (y - 0) + ' ' + (z - 2 * zMod) + ' ' + (x + 4 * xMod) + ' ' + (y - 0) + ' ' + (z - 2 * zMod) + ' minecraft:air replace minecraft:stone_bricks\n';
+        code += 'fill ' + (x + 2 * xMod) + ' ' + (y - 1) + ' ' + (z - 1 * zMod) + ' ' + (x + 4 * xMod) + ' ' + (y - 1) + ' ' + (z - 1 * zMod) + ' minecraft:air replace minecraft:stone_bricks\n';
+        code += "\n";
 
-    code += 'setblock ' + (x+2) + ' ' + (y) + ' ' + (z-2) + ' minecraft:stone_brick_stairs[facing=north] keep\n';
-    code += 'setblock ' + (x+3) + ' ' + (y) + ' ' + (z-2) + ' minecraft:stone_brick_stairs[facing=north] keep\n';
-    code += 'setblock ' + (x+4) + ' ' + (y) + ' ' + (z-2) + ' minecraft:stone_brick_stairs[facing=north] keep\n';
+        code += 'setblock ' + (x - 1 * xMod) + ' ' + (y + 1) + ' ' + (z + 0 * zMod) + ' minecraft:torch keep\n';
+        code += 'setblock ' + (x - 1 * xMod) + ' ' + (y - 1) + ' ' + (z + 1 * zMod) + ' minecraft:wall_torch[facing=' + rotDir[2] + '] keep\n';
+        code += "\n";
 
+        code += 'setblock ' + (x + 0 * xMod) + ' ' + (y - 0) + ' ' + (z + 0 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[3] + '] keep\n';
+        code += 'setblock ' + (x + 1 * xMod) + ' ' + (y - 1) + ' ' + (z + 0 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[3] + '] keep\n';
+        code += 'setblock ' + (x + 1 * xMod) + ' ' + (y - 0) + ' ' + (z - 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[3] + '] keep\n';
+        code += 'setblock ' + (x + 1 * xMod) + ' ' + (y - 3) + ' ' + (z + 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[1] + '] keep\n';
+
+        code += 'setblock ' + (x + 6 * xMod) + ' ' + (y - 0) + ' ' + (z + 0 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[1] + '] keep\n';
+        code += 'setblock ' + (x + 5 * xMod) + ' ' + (y - 1) + ' ' + (z + 0 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[1] + '] keep\n';
+        code += 'setblock ' + (x + 5 * xMod) + ' ' + (y - 0) + ' ' + (z - 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[1] + '] keep\n';
+        code += 'setblock ' + (x + 5 * xMod) + ' ' + (y - 3) + ' ' + (z + 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[3] + '] keep\n';
+
+        code += "\n";
+        code += 'setblock ' + (x + 2 * xMod) + ' ' + (y - 3) + ' ' + (z + 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 3 * xMod) + ' ' + (y - 3) + ' ' + (z + 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 4 * xMod) + ' ' + (y - 3) + ' ' + (z + 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+
+        code += 'setblock ' + (x + 2 * xMod) + ' ' + (y - 2) + ' ' + (z + 0 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 3 * xMod) + ' ' + (y - 2) + ' ' + (z + 0 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 4 * xMod) + ' ' + (y - 2) + ' ' + (z + 0 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+
+        code += 'setblock ' + (x + 2 * xMod) + ' ' + (y - 1) + ' ' + (z - 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 3 * xMod) + ' ' + (y - 1) + ' ' + (z - 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 4 * xMod) + ' ' + (y - 1) + ' ' + (z - 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+
+        code += 'setblock ' + (x + 2 * xMod) + ' ' + (y - 0) + ' ' + (z - 2 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 3 * xMod) + ' ' + (y - 0) + ' ' + (z - 2 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 4 * xMod) + ' ' + (y - 0) + ' ' + (z - 2 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+    }
+else
+    {
+        code += 'fill ' + (x + 0 * xMod) + ' ' + (y - 0) + ' ' + (z + 0 * zMod) + ' ' + (x + 0 * xMod) + ' ' + (y - 0) + ' ' + (z + 6 * zMod) + ' minecraft:air replace minecraft:stone_bricks\n';
+        code += 'fill ' + (x + 0 * xMod) + ' ' + (y - 1) + ' ' + (z + 1 * zMod) + ' ' + (x + 0 * xMod) + ' ' + (y - 1) + ' ' + (z + 5 * zMod) + ' minecraft:air replace minecraft:stone_bricks\n';
+        code += 'fill ' + (x + 0 * xMod) + ' ' + (y - 2) + ' ' + (z + 2 * zMod) + ' ' + (x + 0 * xMod) + ' ' + (y - 2) + ' ' + (z + 4 * zMod) + ' minecraft:air replace minecraft:stone_bricks\n';
+        code += 'fill ' + (x + 1 * xMod) + ' ' + (y - 0) + ' ' + (z + 1 * zMod) + ' ' + (x + 1 * xMod) + ' ' + (y - 0) + ' ' + (z + 5 * zMod) + ' minecraft:air replace minecraft:stone_bricks\n';
+        code += 'fill ' + (x + 2 * xMod) + ' ' + (y - 0) + ' ' + (z + 2 * zMod) + ' ' + (x + 2 * xMod) + ' ' + (y - 0) + ' ' + (z + 4 * zMod) + ' minecraft:air replace minecraft:stone_bricks\n';
+        code += 'fill ' + (x + 1 * xMod) + ' ' + (y - 1) + ' ' + (z + 2 * zMod) + ' ' + (x + 1 * xMod) + ' ' + (y - 1) + ' ' + (z + 4 * zMod) + ' minecraft:air replace minecraft:stone_bricks\n';
+        code += "\n";
+
+        code += 'setblock ' + (x + 0 * xMod) + ' ' + (y + 1) + ' ' + (z - 1 * zMod) + ' minecraft:torch keep\n';
+        code += 'setblock ' + (x - 1 * xMod) + ' ' + (y - 1) + ' ' + (z - 1 * zMod) + ' minecraft:wall_torch[facing=' + rotDir[2] + '] keep\n';
+        code += "\n";
+
+        code += 'setblock ' + (x + 0 * xMod) + ' ' + (y - 0) + ' ' + (z + 0 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[3] + '] keep\n';
+        code += 'setblock ' + (x + 0 * xMod) + ' ' + (y - 1) + ' ' + (z + 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[3] + '] keep\n';
+        code += 'setblock ' + (x + 1 * xMod) + ' ' + (y - 0) + ' ' + (z + 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[3] + '] keep\n';
+        code += 'setblock ' + (x - 1 * xMod) + ' ' + (y - 3) + ' ' + (z + 1 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[1] + '] keep\n';
+
+        code += 'setblock ' + (x + 0 * xMod) + ' ' + (y - 0) + ' ' + (z + 6 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[1] + '] keep\n';
+        code += 'setblock ' + (x + 0 * xMod) + ' ' + (y - 1) + ' ' + (z + 5 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[1] + '] keep\n';
+        code += 'setblock ' + (x + 1 * xMod) + ' ' + (y - 0) + ' ' + (z + 5 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[1] + '] keep\n';
+        code += 'setblock ' + (x - 1 * xMod) + ' ' + (y - 3) + ' ' + (z + 5 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[3] + '] keep\n';
+
+        code += "\n";
+        code += 'setblock ' + (x - 1 * xMod) + ' ' + (y - 3) + ' ' + (z + 2 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x - 1 * xMod) + ' ' + (y - 3) + ' ' + (z + 3 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x - 1 * xMod) + ' ' + (y - 3) + ' ' + (z + 4 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+
+        code += 'setblock ' + (x + 0 * xMod) + ' ' + (y - 2) + ' ' + (z + 2 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 0 * xMod) + ' ' + (y - 2) + ' ' + (z + 3 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 0 * xMod) + ' ' + (y - 2) + ' ' + (z + 4 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+
+        code += 'setblock ' + (x + 1 * xMod) + ' ' + (y - 1) + ' ' + (z + 2 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 1 * xMod) + ' ' + (y - 1) + ' ' + (z + 3 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 1 * xMod) + ' ' + (y - 1) + ' ' + (z + 4 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+
+        code += 'setblock ' + (x + 2 * xMod) + ' ' + (y - 0) + ' ' + (z + 2 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 2 * xMod) + ' ' + (y - 0) + ' ' + (z + 3 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+        code += 'setblock ' + (x + 2 * xMod) + ' ' + (y - 0) + ' ' + (z + 4 * zMod) + ' minecraft:stone_brick_stairs[facing=' + rotDir[0] + '] keep\n';
+    }
     return code;
 }
 
@@ -98,7 +249,7 @@ function boxRoom() {
         codeOutput = "Length times width must be less than 32767. Please adjust values and try again.";
     } else {
         // Because width/2 is stored as a double, Math.ceil produces unexpected results. So handle even widths manually.
-        if (lengthX % 2 == 1) {
+        if (lengthX % 2 === 1) {
             minX = centerX - Math.floor(lengthX / 2);
             maxX = centerX + Math.floor(lengthX / 2);
         } else {
@@ -109,7 +260,7 @@ function boxRoom() {
         minY = floorY;
         maxY = minY + heightY;
 
-        if (widthZ % 2 == 1) {
+        if (widthZ % 2 === 1) {
             minZ = centerZ - Math.floor(widthZ / 2);
             maxZ = centerZ + Math.floor(widthZ / 2);
         } else {
